0b33f11c5c1987031935d397e64f7f049a846ae5
Adris <adrisj7@gmail.com>
2017-12-06 23:34:51 -0500

    Fix weird access issue

--- semting.c
+++ semting.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv) {
             printf("Expected 2, got %d\n", argc - 1);
             exit(1);
         }
-        int sem = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL);
+        int sem = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
         if (sem == -1) {
             printf("Cannot create semaphore:");
             printf("Semaphore already exists!\n");
@@ -32,7 +32,7 @@ int main(int argc, char **argv) {
         printf("Set value of sem to %d\n", val);
         exit(0);
     }
-    
+
     // if viewing semaphore value
     if (!strcmp(argv[1], "-v")) {
         int sem = semget(SEM_KEY, 0, 0);
